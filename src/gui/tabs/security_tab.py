5011646d9b03b79fb7c5d2b3128b087c45e0a4f0
Lee Hayward <50077866+haywardgg@users.noreply.github.com>
2026-02-07 10:33:42 +0000

    Remove header creation from security tab

--- src/gui/tabs/security_tab.py
+++ src/gui/tabs/security_tab.py
@@ -26,7 +26,6 @@ class SecurityTab:
         self.parent.grid_rowconfigure(1, weight=1)
         self.parent.grid_columnconfigure(0, weight=1)
 
-        self._create_header()
         self._create_content()
 
         logger.info("Security tab initialized")
